69cb45910a6bed736f34900f7e746b2bcaa2d536
nphan03 <nphan03.145@gmail.com>
2022-02-15 23:15:16 -0800

    update

--- src/Content.js
+++ src/Content.js
@@ -3,11 +3,9 @@ import React from "react";
 class Content extends React.Component {
     
     render(){
-        console.log("Hello");
-        console.log(this.props);
         return <div className="content">
             <h2>{this.props.title}</h2>
-            {(this.props.media_type == "video") ? <iframe width="420" height="315" src={this.props.url}></iframe>   
+            {(this.props.media_type === "video") ? <iframe width="420" height="315" src={this.props.url}></iframe>   
                 : <img src={this.props.url} alt={this.props.title}/>
             }
             <p><b>Copyright:</b> {this.props.copyright}</p>
