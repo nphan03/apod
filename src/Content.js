8634289f1ea2b431ff085be75fc079d94ef552e7
nphan03 <nphan03.145@gmail.com>
2022-02-15 21:57:44 -0800

    update to display video

--- src/Content.js
+++ src/Content.js
@@ -3,6 +3,8 @@ import React from "react";
 class Content extends React.Component {
     
     render(){
+        console.log(this.props.media_type);
+        console.log(this.props.explanation);
         return <div className="content">
             <h2>{this.props.title}</h2>
             {(this.props.media_type == "video") ? <iframe width="420" height="315" src={this.props.url}></iframe>   
