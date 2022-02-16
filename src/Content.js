a0ec8d954d8e8db89dca0079929f7832896c446a
nphan03 <nphan03.145@gmail.com>
2022-02-15 21:48:58 -0800

    update to display video

--- src/Content.js
+++ src/Content.js
@@ -5,8 +5,8 @@ class Content extends React.Component {
     render(){
         return <div className="content">
             <h2>{this.props.title}</h2>
-            {this.props.media_type == "video" ? <iframe width="420" height="315" src={this.props.url}></iframe>   
-                : <img src={this.props.url} alt={this.props.title}/>
+            {this.props.media_type == "video" && <iframe width="420" height="315" src={this.props.url}></iframe>   
+                || <img src={this.props.url} alt={this.props.title}/>
             }
             <p><b>Copyright:</b> {this.props.copyright}</p>
             <p>{this.props.explanation}</p>
